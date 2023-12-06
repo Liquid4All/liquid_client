fdf43019b8d64c1a16e2f7d4df7f41c0e1b6cbdb
Mathias Lechner <mathias.lechner@hotmail.com>
2023-12-06 11:25:41 -0800

    Code clone detection -> Changed to zero-shot prompting -> 80% accuracy

--- examples/code_clone_detection.py
+++ examples/code_clone_detection.py
@@ -1,51 +1,27 @@
-
 from datasets import load_dataset
 import liquidai
 from tqdm import tqdm
 
 
-def get_prompt(dataset):
-    preprompt = """
-    You are a binary classfier to check the two functions provided are semantic equivalent or not. Given func1 and func2, answer either "True" or "False". Do not output anything else.
-    ==========
-    Examples:
-    """
-    examples = []
-    for i, row in enumerate(dataset):
-        if len(examples) >= 5:
-            break
-        examples.append(
-            f"""
-            Example {i}:
-            func1: {str(row["func1"])}
-            func2: {str(row["func2"])}
-            Answer: {str(row["label"])}
-                        """)
-    example_prompt = "\n\n".join(examples)
-    end_prompt = """
-        ==================
-        Now start:
-    """
-    return preprompt + example_prompt + end_prompt
-
-
 def create_qa(row):
-    return f"""
+    return (
+        f"""
             func1: {str(row["func1"])}
             func2: {str(row["func2"])}
             Answer:
-        """, row["label"]
+        """,
+        row["label"],
+    )
 
 
 def main():
     # Don't forget to set the LIQUID_URL and LIQUID_API_KEY environment variable
     client = liquidai.Client()
-    dataset = load_dataset(
-        "code_x_glue_cc_clone_detection_big_clone_bench")
-    traning_set = dataset["train"]
-    preprompt = get_prompt(traning_set)
+    dataset = load_dataset("code_x_glue_cc_clone_detection_big_clone_bench")
     test_set = dataset["test"]
-
+    preprompt = """
+    Given the code of two functions 'func2' and 'func2', determine whether they implement the same function or not. Answer either "True" or "False". Do not output anything else.
+    """
     num_correct = 0
     num_total = 0
     print(len(test_set))
@@ -59,8 +35,6 @@ def main():
         llm_answer = response["message"]["content"]
         llm_answer = str(llm_answer.replace(" ", ""))
         label_answer = str(a)
-        print(
-            f"llm_answer: {llm_answer} {type(llm_answer)}, label answer: {label_answer} {type(label_answer)}")
         if llm_answer == label_answer:
             num_correct += 1
         num_total += 1
